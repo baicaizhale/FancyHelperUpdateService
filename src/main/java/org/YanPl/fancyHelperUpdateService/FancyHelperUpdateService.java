8e18472deb9955f70681a97d9985311aa2514183
Yan Cai <2651557041@qq.com>
2026-02-15 16:17:27 +0800

    init

--- src/main/java/org/YanPl/fancyHelperUpdateService/FancyHelperUpdateService.java
+++ src/main/java/org/YanPl/fancyHelperUpdateService/FancyHelperUpdateService.java
@@ -1,17 +1,31 @@
 package org.YanPl.fancyHelperUpdateService;
 
+import org.YanPl.fancyHelperUpdateService.util.VersionUtil;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FancyHelperUpdateService extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        // 检测服务器版本
+        String version = VersionUtil.getVersionString();
+        getLogger().info("检测到服务器版本: " + version);
+        
+        // 检查版本是否支持
+        if (!VersionUtil.isSupported()) {
+            getLogger().warning("当前服务器版本不在支持范围内！");
+            getLogger().warning("本插件支持版本: 1.18 - 1.21");
+            getLogger().warning("当前版本: " + version);
+            getLogger().warning("插件可能无法正常工作，请谨慎使用！");
+        } else {
+            getLogger().info("服务器版本兼容性检查通过！");
+        }
+        
+        getLogger().info("FancyHelperUpdateService 已启动！");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("FancyHelperUpdateService 已关闭！");
     }
 }
